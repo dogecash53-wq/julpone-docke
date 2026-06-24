82a03b67e6b2a8746bd556c61e361d915e75d4b4
dogecash53-wq <dogecash53@gmail.com>
2026-06-25 00:57:34 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 1. Awtomatikong palitan ang listen 8080 ng Nginx tungong 8081
-sed -i 's/listen 8080/listen 8081/g' /etc/nginx/nginx.conf
+sed -i 's/listen 8080/listen 8085/g' /etc/nginx/nginx.conf
 
 # 2. I-start ang Xray (panares)
 /usr/bin/panares -config /etc/xray/config.json &
