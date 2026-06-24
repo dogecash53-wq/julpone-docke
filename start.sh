fd918d877859d940c93e9abb6e0df67f5479c038
dogecash53-wq <dogecash53@gmail.com>
2026-06-25 02:53:13 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
-
-# Palitan ang port sa haproxy config bago i-start gamit ang $PORT variable
-sed -i "s/8080/$PORT/g" /etc/haproxy/haproxy.cfg
-haproxy -f /etc/haproxy/haproxy.cfg -db
-
-# 2. I-start ang Xray (panares)
+# Start Xray
 /usr/bin/panares -config /etc/xray/config.json &
 
-# 3. I-start ang Nginx sa background
-nginx -g 'daemon off;' &
-
-# 4. I-start ang HAProxy (ang magsisilbing gateway sa 8080)
-haproxy -f /etc/haproxy/haproxy.cfg -db
+# Start Nginx (Direkta sa 8080)
+# Wala nang HAProxy kaya walang port conflict
+nginx -g 'daemon off;'
