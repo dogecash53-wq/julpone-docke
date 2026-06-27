692394b7c36f8d250b34bc0605e49646cf19e0c3
dogecash53-wq <dogecash53@gmail.com>
2026-06-27 18:57:01 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,15 +1,6 @@
 #!/bin/sh
-
-# 1. Start Xray/Panares sa background
+# Start Xray sa background
 /usr/bin/panares -config /etc/xray/config.json > /dev/null 2>&1 &
-echo "Xray started..."
-sleep 3
 
-# 2. Start HAProxy sa background
-haproxy -f /etc/haproxy/haproxy.cfg -D
-echo "HAProxy started..."
-sleep 2
-
-# 3. Start Nginx sa foreground (HULI ITO!)
-echo "Starting Nginx..."
+# Start Nginx sa foreground
 nginx -g "daemon off;"
